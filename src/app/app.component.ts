fc14df79a3db8a7888a8bd4b33b7ddd990c8b4c6
Abhii27 <abhishekpanchal@1998>
2022-01-10 22:17:32 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PipesDemo';
+  
+  name: string  = '';
+  date: string = '';
+  amount!: number;
+  height!: number;
+  miles!: number;
+  
+
+  onMilesChanged(event:any){
+    this.miles = parseFloat(event.target.value);
+  }
+
+  onHeightChange(event:any){
+    this.height = parseFloat(event.target.value);
+  }
+  
+  onNameChange(event:any){
+    this.name = event.target.value;
+  }
+
+  onDateChange(event:any){
+    this.date = event.target.value;
+  }
+
+  onAmountChange(event:any){
+    this.amount = parseFloat(event.target.value);
+  }
+
+  car = {
+    make : 'Toyota',
+    model: 'Camry',
+    year: 2000
+  };
 }
